e6497f28876effdcdcd113809286ca7f9b64a038
Jormatosijorma <joonas@vilkas.fi>
2018-05-24 10:41:09 +0300

    Bump version: 0.2.0 → 0.2.1

--- procountor/__init__.py
+++ procountor/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Joonas Maliniemi"""
 __email__ = 'joonas@vilkas.fi'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='Procountor',
-    version='0.2.0',
+    version='0.2.1',
     description="Python library for calling Procountor REST API",
     long_description=readme + '\n\n' + history,
     author="Joonas Maliniemi",
