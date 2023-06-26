9773c3bb550b0a55d522b419dd5dc9c3d61d9b2e
Tatu Wikman <tatu.wikman@gmail.com>
2023-06-26 14:22:01 +0300

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='Procountor',
-    version='2.3.1',
+    version='2.3.2',
     description="Python library for calling Procountor REST API",
     long_description=readme + '\n\n' + history,
     author="Joonas Maliniemi",
